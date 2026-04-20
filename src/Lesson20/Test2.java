9e3990515a882c90a8c0546bd30812d1ad82166b
kondakowa <kondakowa.xenya2015@yandex.ru>
2026-04-20 14:39:30 +0300

    test2

---
+++ src/Lesson20/Test2.java
@@ -0,0 +1,16 @@
+package Lesson20;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("poka");
+list.add("privet");
+list.add("ok");
+list.add(1, "hello");
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+    }
+}
