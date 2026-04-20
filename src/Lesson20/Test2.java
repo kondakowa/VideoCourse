43eb65ede23f30f15825e2e3e1030e13bcbe3c89
kondakowa <kondakowa.xenya2015@yandex.ru>
2026-04-20 14:48:55 +0300

    system.out.println

--- src/Lesson20/Test2.java
+++ src/Lesson20/Test2.java
@@ -3,14 +3,19 @@ package Lesson20;
 import java.util.ArrayList;
 
 public class Test2 {
+
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("poka");
-list.add("privet");
-list.add("ok");
-list.add(1, "hello");
+        list.add("privet");
+        list.add("ok");
+        list.add(1, "hello");
         for (String s : list) {
             System.out.print(s + " ");
+        }
+        System.out.println();
+        for (int i=0;i<list.size();i++){
+        System.out.println(list.get(i));
         }
     }
 }
