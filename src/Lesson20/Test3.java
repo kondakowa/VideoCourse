22c21e8739e5a233b9487e4f8220a505b5e4fbef
kondakowa <kondakowa.xenya2015@yandex.ru>
2026-04-20 15:07:51 +0300

    list.remove

--- src/Lesson20/Test3.java
+++ src/Lesson20/Test3.java
@@ -1,20 +1,29 @@
-
 package Lesson20;
-import java.util.ArrayList;
-public class Test3 {
-    public static void main(String[] args) {
-    ArrayList <StringBuilder> list = new ArrayList<>();
-    StringBuilder sb1 = new StringBuilder("Hello");
-        StringBuilder sb2 = new StringBuilder("ok");
-    StringBuilder sb3 = new StringBuilder("privet");
-    list.add(sb1);
-    list.add(sb2);
-    list.add(sb3);
-for(int i=0;i<list.size();i++){
-    list.get(i).append("!!!");
-    }
-    for(StringBuilder sb:list){
-System.out.print(sb+" ");
 
+import java.util.ArrayList;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        ArrayList<StringBuilder> list = new ArrayList<>();
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("ok");
+        StringBuilder sb3 = new StringBuilder("privet");
+        list.add(sb1);
+        list.add(sb2);
+        list.add(sb3);
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).append("!!!");
+        }
+        for (StringBuilder sb : list) {
+            System.out.print(sb + " ");
+
+        }
+        
+         System.out.println();
+        list.remove(2);
+         for (StringBuilder sb : list) {
+            System.out.print(sb + " ");
+         }
+    }
 }
-}}
