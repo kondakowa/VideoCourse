9ccfecde247427c04d3f15575134a8c9677f33ae
kondakowa <kondakowa.xenya2015@yandex.ru>
2026-04-20 16:49:57 +0300

    metod Collections.sort

---
+++ src/Lesson20/Test7.java
@@ -0,0 +1,23 @@
+package Lesson20;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test7 {
+
+    public static void main(String[] args) {
+        String s1 = "A";
+        String s2 = "B";
+        String s3 = "C";
+        String s4 = "D";
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add(s3);
+        list1.add(s1);
+        list1.add(s4);
+        list1.add(s2);
+        System.out.println(list1);
+        Collections.sort(list1);
+                System.out.println(list1);
+
+    }
+}
